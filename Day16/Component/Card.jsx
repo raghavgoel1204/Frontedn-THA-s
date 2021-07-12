6a52db7687ffe54e54f947a97c83ad974b11e800
Raghav Goel <49436491+raghavgoel1204@users.noreply.github.com>
2021-07-12 17:30:00 +0530

    Create Card.jsx

---
+++ Day16/Component/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+    return(
+        <div className="card">
+            <img src="https://wallpapercave.com/wp/wp1956137.png" alt="" />
+            <h3>This is Levi Ackerman From Attack On Titan.</h3>
+        </div>
+    );
+};
+
+export default Card;
